a343c0f0d4b762dee5960aa3b77ae715d40ca01c
GursimranjeetSingh <gursimranjeets@2be.ae>
2024-07-10 10:33:58 +0530

    new data

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from encryption_code import encode_files,decode_files
 
 #secret_data = "18-2-2023" #iip date structure
 #secret_data = "15000 31-10-2023" #dewa date structure
+#secret_data = "200 15-04-2026" #punjabi government date structure
+#secret_data = "200 15000 15-04-2026" #qcomm signage structure   #screen_count user_count expiry_date
 
-secret_data="200 31-10-2024"
+secret_data="15 15-07-2024"
 
-# encode_files(secret_data)
-decode_files()
+encode_files(secret_data)
+# decode_files()
